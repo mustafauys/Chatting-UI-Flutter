8308f999d64d1a44cf0c83d33ffc222d60dbe2fd
Mustafa UYSAL <18701031@mersin.edu.tr>
2021-03-19 23:30:09 +0300

    Users Json File Edited

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -14,26 +14,26 @@ final User currentUser =
     User(id: 0, name: 'You', avatar: 'assets/images/Addison.jpg');
 
 final User addison =
-    User(id: 1, name: 'Addison', avatar: 'assets/images/Addison.jpg');
+    User(id: 1, name: 'Melek', avatar: 'assets/images/Addison.jpg');
 
-final User angel =
-    User(id: 2, name: 'Angel', avatar: 'assets/images/Angel.jpg');
+final User angel = User(id: 2, name: 'Aslı', avatar: 'assets/images/Angel.jpg');
 
 final User deanna =
-    User(id: 3, name: 'Deanna', avatar: 'assets/images/Deanna.jpg');
+    User(id: 3, name: 'Melis', avatar: 'assets/images/Deanna.jpg');
 
-final User jason = User(id: 4, name: 'Json', avatar: 'assets/images/Jason.jpg');
+final User jason =
+    User(id: 4, name: 'Burak', avatar: 'assets/images/Jason.jpg');
 
-final User judd = User(id: 5, name: 'Judd', avatar: 'assets/images/Judd.jpg');
+final User judd = User(id: 5, name: 'Mehmet', avatar: 'assets/images/Judd.jpg');
 
 final User leslie =
-    User(id: 6, name: 'Leslie', avatar: 'assets/images/Leslie.jpg');
+    User(id: 6, name: 'Nihal', avatar: 'assets/images/Leslie.jpg');
 
 final User nathan =
-    User(id: 7, name: 'Nathan', avatar: 'assets/images/Nathan.jpg');
+    User(id: 7, name: 'Nazlı', avatar: 'assets/images/Nathan.jpg');
 
 final User stanley =
-    User(id: 8, name: 'Stanley', avatar: 'assets/images/Stanley.jpg');
+    User(id: 8, name: 'Doruk', avatar: 'assets/images/Stanley.jpg');
 
 final User virgil =
-    User(id: 9, name: 'Virgil', avatar: 'assets/images/Virgil.jpg');
+    User(id: 9, name: 'Rüzgar', avatar: 'assets/images/Virgil.jpg');

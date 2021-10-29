7cb99805b9647edcf51bf9bc63ab4f3c5d127a99
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-10-29 23:10:33 +0300

    Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,11 @@ void main() {
     
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
